360a0863a199327efddf167e4f9829bb833bac51
Guilherme Bracero Gonzales <guibragon@gmail.com>
2022-08-16 15:08:10 -0300

    feat: API views are half done

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Models import 
+from core.models import BuildNameModel, BuildModel
+
+# DRF / Serializers
+from rest_framework import generics
+from core.serializers import BuildNameSerializer, BuildModelSerializer
+
+# Land Page
+class IndexView(TemplateView):
+    template_name: str = "index.html"
 
 
+class BuildNameAPI(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = BuildNameSerializer
+    queryset = BuildNameModel.objects.all()
+
+
+class BuildAPI(generics.ListAPIView):
+    serializer_class = BuildModelSerializer
+    queryset = BuildModel.objects.all()
